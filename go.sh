73ced7fdc31f150565de7b582176f09b06a7021b
Sterling Anderson <s@sterlinganderson.net>
2018-02-08 16:04:17 -0600

    initial

---
+++ go.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cp configs/.vimrc ~/
+
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/swaps
+mkdir -p ~/.vim/undo
+
+if [ `uname` == Darwin ]; then
+	brew install base16-manager
+elif [ `uname` == Linux ]; then
+	git clone https://github.com/AuditeMarlow/base16-manager.git
+	cd base16-manager
+	sudo make install
+fi
+
+base16-manager install chriskempson/base16
+base16-manager install chriskempson/base16-shell
+
+base16-manager set tomorrow-dark
